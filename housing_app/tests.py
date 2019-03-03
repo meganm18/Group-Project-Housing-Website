aee49d3a1afc9ad8b671e74eb3bbc884560ed350
Megan Marshall <mem5ak@virginia.edu>
2019-03-03 10:55:20 -0500

    Update tests.py
    Added tests for saving apartments

--- housing_app/tests.py
+++ housing_app/tests.py
@@ -13,3 +13,19 @@ class ApartmentTestCase(TestCase):
         apartment_2 = Apartment.objects.get(name="Apartment 2")
         self.assertEqual(apartment_1.company, 'company 1')
         self.assertEqual(apartment_2.company, 'company 2')
+
+class SavedListTestCase(TestCase):
+    def setUp(self):
+        Apartment.objects.create(name="Apartment 1", company="company 1", location="location 1", price=1000, size=1000,
+                                 bedrooms=1, furnished="yes", pets="yes")
+        Apartment.objects.create(name="Apartment 2", company="company 2", location="location 2", price=2000, size=2000,
+                                 bedrooms=2, furnished="no", pets="no")
+        Profile.objects.create(user="user 1", bio="hello")
+    def test_favorites(self):
+        apartment_1 = Apartment.objects.get(name="Apartment 1")
+        apartment_2 = Apartment.objects.get(name="Apartment 2")
+        profile_1 = Profile.objects.get(name="user 1")
+        profile_1.favorites.add(apartment_1)
+        self.assertEqual(profile_1.favorites.name, "Apartment 1")
+        profile_1.favorites.add(apartment_2)
+        self.assertEqual(profile_1.favorites.name, ["Apartment 1", "Apartment 2"])
