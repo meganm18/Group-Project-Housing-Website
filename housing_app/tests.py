0da6322c330a1c6ab01214dd8f5c1fbfac474b8e
Megan Marshall <mem5ak@virginia.edu>
2019-03-03 11:25:12 -0500

    Update tests.py

--- housing_app/tests.py
+++ housing_app/tests.py
@@ -21,8 +21,7 @@ class SavedListTestCase(TestCase):
                                  bedrooms=1, furnished="yes", pets="yes")
         Apartment.objects.create(name="Apartment 2", company="company 2", location="location 2", price=2000, size=2000,
                                  bedrooms=2, furnished="no", pets="no")
-        user1 = User.objects.create_user(username = "user 1")
-        Profile.objects.create(user=user1, bio="hello")
+        Profile.objects.create(bio="hello")
     def test_favorites(self):
         apartment_1 = Apartment.objects.get(name="Apartment 1")
         apartment_2 = Apartment.objects.get(name="Apartment 2")
