e488d62e44d0714943d66d2379415ee0d9cd5b14
Megan Marshall <mem5ak@virginia.edu>
2019-03-03 12:43:01 -0500

    Update tests.py

--- housing_app/tests.py
+++ housing_app/tests.py
@@ -23,15 +23,13 @@ class SavedListTestCase(TestCase):
         Apartment.objects.create(name="Apartment 2", company="company 2", location="location 2", price=2000, size=2000,
                                  bedrooms=2, furnished="no", pets="no")
         self.user1 = User.objects.create_user(username="user 1")
-        Profile.objects.create(user = self.user1, bio="hello")
-        self.factory = RequestFactory()
+        self.user1.save()
+        self.profile_1 = Profile.objects.create(user = self.user1, bio="hello")
+        self.profile_1.save()
     def test_favorites(self):
-        request = self.factory.get(r'^favorites/')
-        request.user = self.user1
         apartment_1 = Apartment.objects.get(name="Apartment 1")
         apartment_2 = Apartment.objects.get(name="Apartment 2")
-        profile_1 = Profile.objects.get(bio="hello")
-        profile_1.favorites.add(apartment_1)
-        self.assertEqual(profile_1.favorites.name, "Apartment 1")
-        profile_1.favorites.add(apartment_2)
-        self.assertEqual(profile_1.favorites.name, ["Apartment 1", "Apartment 2"])
+        self.profile_1.favorites.add(apartment_1)
+        self.assertEqual(self.profile_1.favorites.name, "Apartment 1")
+        self.profile_1.favorites.add(apartment_2)
+        self.assertEqual(self.profile_1.favorites.name, ["Apartment 1", "Apartment 2"])
