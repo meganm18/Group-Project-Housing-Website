e0ae50801d69dd7a54bfb45d6ad51e0c63cf0db2
Megan Marshall <mem5ak@virginia.edu>
2019-03-03 11:06:50 -0500

    Update tests.py

--- housing_app/tests.py
+++ housing_app/tests.py
@@ -21,12 +21,12 @@ class SavedListTestCase(TestCase):
                                  bedrooms=1, furnished="yes", pets="yes")
         Apartment.objects.create(name="Apartment 2", company="company 2", location="location 2", price=2000, size=2000,
                                  bedrooms=2, furnished="no", pets="no")
-        user1 = User.objects.create_user(username = 'user 1')
+        user1 = User.objects.create_user(username = "user 1")
         Profile.objects.create(user=user1, bio="hello")
     def test_favorites(self):
         apartment_1 = Apartment.objects.get(name="Apartment 1")
         apartment_2 = Apartment.objects.get(name="Apartment 2")
-        profile_1 = Profile.objects.get(name="user 1")
+        profile_1 = Profile.objects.get(bio="hello")
         profile_1.favorites.add(apartment_1)
         self.assertEqual(profile_1.favorites.name, "Apartment 1")
         profile_1.favorites.add(apartment_2)
