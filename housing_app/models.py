1d8cda546867601f79777d3a9c284227a39d6ec9
Megan Marshall <mem5ak@virginia.edu>
2019-03-24 19:15:43 -0400

    Update models.py

--- housing_app/models.py
+++ housing_app/models.py
@@ -45,9 +45,9 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
 	if kwargs.get('created', False):
-		User.Profile.objects.create(user=kwargs['instance'])
-    #if created:
-    #    UserProfile.objects.create(user=instance)
+		User.UserProfile.objects.create(user=kwargs['instance'])
+	# if created:
+	#    UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
