14731270320fe0e2a09daf44c7f5089584547ad9
Megan Marshall <mem5ak@virginia.edu>
2019-03-24 12:13:43 -0400

    Update models.py
    Working on Heroku logging in

--- housing_app/models.py
+++ housing_app/models.py
@@ -43,10 +43,10 @@ class UserProfile(models.Model):
 # The following receivers save data to the user profile whenever changes are made
 # code retrieved from : https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_userprofile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_user_userprofile(sender, instance, **kwargs):
     instance.userprofile.save()
