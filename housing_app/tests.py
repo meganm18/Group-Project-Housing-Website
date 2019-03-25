365838a97edcedc947911a241b51bf1320926aac
Megan Marshall <mem5ak@virginia.edu>
2019-03-25 09:28:04 -0400

    Update tests.py
    Added a test case

--- housing_app/tests.py
+++ housing_app/tests.py
@@ -16,7 +16,7 @@ class ApartmentTestCase(TestCase):
         self.assertEqual(apartment_2.company, 'company 2')
 
 
-class SavedListTestCase(TestCase):
+class SavedFavTestCase(TestCase):
     def setUp(self):
         Apartment.objects.create(name="Apartment 1", company="company 1", location="location 1", price=1000, size=1000,
                                  bedrooms=1, furnished="yes", pets="yes")
@@ -41,6 +41,33 @@ class SavedListTestCase(TestCase):
         self.profile_1.favorites.add(apartment_2)
         self.profile_1.save()
         self.assertEqual(self.profile_1.favorites.all()[1].name, "Apartment 2")
+
+class DeleteFavTestCase(TestCase):
+    def setUp(self):
+        Apartment.objects.create(name="Apartment 1", company="company 1", location="location 1", price=1000, size=1000,
+                                 bedrooms=1, furnished="yes", pets="yes")
+        Apartment.objects.create(name="Apartment 2", company="company 2", location="location 2", price=2000, size=2000,
+                                 bedrooms=2, furnished="no", pets="no")
+        User.objects.create_user(username="example user 1")
+
+    def test_delete(self):
+        apartment_1 = Apartment.objects.get(name="Apartment 1")
+        apartment_2 = Apartment.objects.get(name="Apartment 2")
+        ex_user1 = User.objects.get(username="example user 1")
+        try:
+            self.profile_1 = UserProfile.objects.get(user_id=1)
+        except:
+            UserProfile.objects.create(user=ex_user1, bio="hello")
+            self.profile_1 = UserProfile.objects.get(bio="hello")
+        self.profile_1.favorites.set = Apartment.objects
+        self.profile_1.favorites.clear()
+        self.profile_1.favorites.add(apartment_1)
+        self.profile_1.save()
+        self.profile_1.favorites.add(apartment_2)
+        self.profile_1.save()
+        self.assertEqual(self.profile_1.favorites.all()[1].name, "Apartment 2")
+        self.profile_1.favorites.delete(apartment_1)
+        self.assertEqual(self.profile_1.favorites.all()[0].name, "Apartment 2")
 
 class ViewPagesTestCase(TestCase):
     #https://docs.djangoproject.com/en/2.1/topics/testing/advanced/
