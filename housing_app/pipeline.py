1435b3a7a4eec07b37e324e8e50d9b821d9e6ce3
Megan Marshall <mem5ak@virginia.edu>
2019-03-24 20:12:11 -0400

    Update pipeline.py

--- housing_app/pipeline.py
+++ housing_app/pipeline.py
@@ -2,7 +2,7 @@ from .models import UserProfile
 
 # code retrieved from https://stackoverflow.com/questions/29575713/retrieving-profile-picture-from-google-and-facebook-in-python-social-auth/29576422
 def get_avatar(backend, strategy, details, response,
-        user=None, *args, **kwargs):
+        UserProfile=None, *args, **kwargs):
     url = None
     if backend.name == 'google-oauth2':
         url = response['picture']
