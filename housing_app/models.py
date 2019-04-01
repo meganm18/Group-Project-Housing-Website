1118891e1807724deb1e9667892fd826669c8b27
Megan Marshall <mem5ak@virginia.edu>
2019-04-01 16:12:05 -0400

    Working on compare page

--- housing_app/models.py
+++ housing_app/models.py
@@ -29,9 +29,9 @@ class UserProfile(models.Model):
 	avatar = models.ImageField(default="static/images/blank_profile.png", max_length=500)
 	bio = models.TextField(max_length=500, blank=True)
 	favorites = models.ManyToManyField(Apartment, blank=True,related_name="favorites")
-	#compare = models.ManyToManyField(Apartment, blank=True, related_name="compare")
-	compare0 = models.ForeignKey(Apartment, blank = True, default = 0, on_delete='SET_DEFAULT', related_name="compare0")
-	compare1 = models.ForeignKey(Apartment, blank = True, default = 0, on_delete='SET_DEFAULT', related_name="compare1")
+	compare = models.ManyToManyField(Apartment, blank=True, related_name="compare")
+	#compare0 = models.ForeignKey(Apartment, blank = True, null=True, on_delete='SET_DEFAULT', related_name="compare0")
+	#compare1 = models.ForeignKey(Apartment, blank = True, null=True, on_delete='SET_DEFAULT', related_name="compare1")
 	def __str__(self):
 		return self.user.username
 
